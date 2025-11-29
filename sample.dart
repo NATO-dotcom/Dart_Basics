433d46770bb15ddd8e880c3396cbf83874d94393
NATO-dotcom <nato114267@students.must.ac.ke>
2025-11-29 23:06:42 +0300

    Explored the basics in dart

--- sample.dart
+++ sample.dart
@@ -12,4 +12,25 @@ void main(){
   String day = "Saturday";
   var year = 2025;
   bool isStudent = true;
+
+  var fruits = ['Apple', 'Banana', 'Mango'];//list
+  var details = {'name': 'John', 'age': 25};//map
+
+  var image = {
+    //address:data
+    'width': 1920,
+    'height': 1080,
+    'color': 'blue',
+    'url': 'https://example.com/image.jpg',
+    'tags': ['nature', 'landscape', 'sunset']
+  };
+
+  //Functions in dart
+  double getArea(int r){
+
+    double area = 1/2 *3.14*r*r;
+    return area;
+  }
+  var Area = getArea(7);
+  print("Area = $Area");
 }
