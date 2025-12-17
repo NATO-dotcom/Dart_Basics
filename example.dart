712ce40bef89722d79515773cf9656eed7f47641
NATO-dotcom <nato114267@students.must.ac.ke>
2025-12-17 13:29:54 +0300

    Add my example on Future implying asnchronous programming

--- example.dart
+++ example.dart
@@ -1,5 +1,15 @@
 //Asynchronous programming enables a dart program to perform multiple tasks simultaneously instead of executing one after another.
-//future
+//futures-results that take some time to complete in asynchronous programming
 //async
 //await
 //stream
+const oneSecond = Duration(seconds: 10);
+Future<void> printWithDelay(String message) async {
+  await Future.delayed(oneSecond);
+  print(message);
+}
+Future<void> main() async {
+  print("Start");
+  await printWithDelay("Hello after delay");
+  print("End");
+}
